b26b9b9c07a817a5e6147774c4d125f335867461
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-08-19 23:30:06 +0545

    changes

--- lib/hello-world.tsx
+++ lib/hello-world.tsx
@@ -3,5 +3,5 @@ export interface IHelloWorldProps {
   className?: string;
 }
 export const HelloWorld = ({ text, className = "" }: IHelloWorldProps) => {
-  return <div className={className}>Hello world = ${text}</div>;
+  return <div className={className}>Hello worlds = ${text}</div>;
 };
