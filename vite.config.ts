680569b9ad3becd832b07558b82a9b8fe1124157
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-08-20 00:51:43 +0545

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     sourcemap: true,
     lib: {
