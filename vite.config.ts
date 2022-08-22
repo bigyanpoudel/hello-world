38ad0d513e31436d38797e31ca504f1bf04a107f
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-08-22 14:33:50 +0545

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
         }
         if (filePath.includes("/dist/component")) {
           return {
-            filePath: filePath.replace("/dist/component", "/component"),
+            filePath: filePath.replace("/component", ""),
             content,
           };
         }
