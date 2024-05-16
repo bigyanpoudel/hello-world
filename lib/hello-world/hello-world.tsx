f0a779a0583e922e84bae6d15abe231b82bed497
bigyanpoudel <bgyanpoudel11@gmail.com>
2024-05-16 12:58:41 +0545

    feat: slight change

--- lib/hello-world/hello-world.tsx
+++ lib/hello-world/hello-world.tsx
@@ -1,5 +1,8 @@
 import { IHelloWorldProps } from "../types";
 
-export const HelloWorld = ({ text, className = "" }: IHelloWorldProps) => {
+export const HelloWorld: React.FC<IHelloWorldProps> = ({
+  text,
+  className = "",
+}: IHelloWorldProps) => {
   return <div className={className}>Hello worlds = {text}</div>;
 };
